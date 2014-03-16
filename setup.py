e5daad399fb6ef2ef61e9eb75db097daba3dcd0b
Yosi Dediashvili <yosi.dediashvili@gmail.com>
2014-03-16 02:01:09 +0200

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name="EasyConfig",
+	version="1.0",
+	packages = ["EasyConfig"],
+	description = "A wrapper class for python's ConfigParser class that represent the configuration items as objects.",
+	author = "Yosi Dediashvili",
+	author_email = "yosi.dediashvili@gmail.com",
+	url = "https://github.com/yosi-dediashvili/EasyConfig",
+	license = "MIT license"
+)
